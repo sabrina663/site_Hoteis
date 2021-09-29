846c040373d94d82de3e5218bfdb5e40f506c233
sabrina <bina.oliver4@gmail.com>
2021-09-29 14:17:10 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let time = 2000; index = 0;
+
+function slide(){
+    let imagem = document.querySelectorAll('#carrousel img');maxIndex = imagem.length;
+    console.log(imagem)
+    imagem[index].classList.remove('show')
+    index ++
+    if(index >= maxIndex){
+        index = 0;
+    }
+    imagem[index].classList.add('show')
+
+}
+
+function start() {
+    setInterval(() => {
+        slide();
+    }, time);
+    
+}
+
+window.addEventListener('load',start)
